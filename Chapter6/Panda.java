7eed7131687b9745f9045fd15577e9f0360277b2
Tran Man <tranman@MacBook-Pro-cua-Tran.local>
2025-12-01 23:45:04 +0700

    example redeclaring private method and hiding static method

---
+++ Chapter6/Panda.java
@@ -0,0 +1,19 @@
+/**
+* Example: Hiding Static Method
+*/
+
+class Bear {
+    public static void eat() {
+        System.out.println("Bear is eating");
+    }
+}
+
+public class Panda extends Bear {
+    public static void eat() {
+        System.out.println("Panda is eating...");
+    }
+
+    public static void main(String[] args) {
+        eat();
+    }
+}
