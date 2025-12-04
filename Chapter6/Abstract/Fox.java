0c39bca319c00c785a7c4a39fe1d20688364a8e8
Mantm <mantm@allexceed.co.jp>
2025-12-04 09:55:41 +0700

    example abstract classes

---
+++ Chapter6/Abstract/Fox.java
@@ -0,0 +1,11 @@
+/**
+ * Example: Abstract class
+ */
+
+public class Fox extends Canine {
+    
+    @Override
+    public String getSound() {
+        return "Fox...Fox...";
+    }
+}
---
+++ Chapter6/Abstract/Wolf.java
@@ -0,0 +1,11 @@
+/**
+ * Example: Abstract class
+ */
+
+public class Wolf extends Canine {
+ 
+    @Override
+    public String getSound() {
+        return "Wolf...Wolf....";
+    }
+}
